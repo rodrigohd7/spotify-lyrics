a34246ef35e4016f2b58e3cae2de721efc0bd132
rodrigor <rodrigor@motorola.com>
2018-02-07 09:26:51 -0200

    Added some error verifications

--- lyrics.py
+++ lyrics.py
@@ -9,9 +9,16 @@ output = subprocess.check_output(['sh', 'lyr.sh'])
 artist = output.split("\n")[0].strip()
 title = output.split("\n")[1].strip()
 
-url = "{0}/{1}/{2}".format(LYRICS_API, urllib.pathname2url(artist), urllib.pathname2url(title))  
+url = "{0}/{1}/{2}".format(LYRICS_API, urllib.pathname2url(artist), urllib.pathname2url(title)).lower()
 
 reponse = requests.get(url=url)
 data = json.loads(reponse.text)
 
-print data['lyric']
+print "{0} - {1} \n".format(artist, title)
+
+if data['err'] == 'none':
+  print data['lyric']
+elif data['err'] == 'not found':
+  print 'Lyrics not found on API'
+else:
+  print data['err']
